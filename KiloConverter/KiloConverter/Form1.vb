b3f96b585277b5db14f82ef8034d1fc911116531
bradymac16 <33064951+bradymac16@users.noreply.github.com>
2017-11-01 10:01:54 -0500

    We updated the Kilo Converter

--- KiloConverter/KiloConverter/Form1.vb
+++ KiloConverter/KiloConverter/Form1.vb
@@ -1,3 +1,23 @@
-﻿Public Class Form1
+﻿Public Class KiloConvertor
 
+    Private Sub btnInches_Click(sender As Object, e As EventArgs) Handles btnInches.Click
+        lblMessage.Text = "1 Kilo = 39,370 inches"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnFeet_Click(sender As Object, e As EventArgs) Handles btnFeet.Click
+        lblMessage.Text = "1 Kilo = 3,281 feet"
+    End Sub
+
+    Private Sub btnYards_Click(sender As Object, e As EventArgs) Handles btnYards.Click
+        lblMessage.Text = "1 Kilo = 1,0903 yards"
+    End Sub
+
+    Private Sub btnMiles_Click(sender As Object, e As EventArgs) Handles btnMiles.Click
+        lblMessage.Text = "1 Kilo = 0.6214 miles"
+    End Sub
 End Class
